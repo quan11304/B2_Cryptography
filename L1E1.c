a5566a0a76cda2754af5ae30d323e2b2bc8f37b2
QuanDellXPS13 <98805733+quan11304@users.noreply.github.com>
2024-01-25 11:15:37 +0700

    L1E1 - I did something but it still doesn't work.

--- L1E1.c
+++ L1E1.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+char x[19] = "1111101010101010101";
+char y[22] = "1100110011001100110011";
+char z[23] = "11100001111000011110000";
+int n = 10;
+
+int shiftX() {
+    int temp = ((int) x[13] - 48) ^ ((int) x[16] - 48)
+               ^ ((int) x[17] - 48) ^ ((int) x[18] - 48);
+    // Starting from the last element, excluding the first element
+    for (int j = 18; j > 0; j--)
+        x[j] = x[j - 1];
+    x[0] = (char) temp;
+}
+int shiftY() {
+    int temp = ((int) y[20] - 48) ^ ((int) y[21] - 48);
+    for (int j = 21; j > 0; j--)
+        y[j] = y[j - 1];
+    y[0] = (char) temp;
+}
+int shiftZ() {
+    int temp = ((int) z[7] - 48) ^ ((int) z[20] - 48)
+               ^ ((int) z[21] - 48) ^ ((int) z[22] - 48);
+    for (int j = 22; j > 0; j--)
+        z[j] = z[j - 1];
+    z[0] = (char) temp;
+}
+
 int main() {
-    char x[19] = "1111101010101010101";
-    char y[22] = "1100110011001100110011";
-    char z[23] = "11100001111000011110000";
-    int n = 10;
+
 
 //	printf("Enter X (19 bits): ");
 //	scanf("%s", x);
@@ -24,23 +48,19 @@ int main() {
         printf("%d", x18 ^ y21 ^ z22);
 
         // Shift
-        if (x[8] == y[10] || x[8] == z[10]) {
-            // Starting from the last element, excluding the first element
-            for (int j = 18; j > 0; j--)
-                x[j] = x[j - 1];
-            x[0] = ((int) x[13] - 48) ^ ((int) x[16] - 48)
-                   ^ ((int) x[17] - 48) ^ ((int) x[18] - 48);
+        if (x[8] == y[10]) {
+            shiftX();
+            shiftY();
+            if (x[8] == z[10])
+                shiftZ();
         }
-        if (y[10] == x[8] || y[10] == z[10]) {
-            for (int j = 21; j > 0; j--)
-                y[j] = y[j - 1];
-            y[0] = ((int) y[20] - 48) ^ ((int) y[21] - 48);
+        else if (x[8] == z[10]) {
+            shiftX();
+            shiftZ();
         }
-        if (z[10] == x[8] || z[10] == y[10]) {
-            for (int j = 22; j > 0; j--)
-                z[j] = z[j - 1];
-            z[0] = ((int) z[7] - 48) ^ ((int) z[20] - 48)
-                   ^ ((int) z[21] - 48) ^ ((int) z[22] - 48);
+        else if (y[10] == z[10]) {
+            shiftY();
+            shiftZ();
         }
     }
 }
