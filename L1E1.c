53f59d7f6b69bdd695c552217052c67278f5c0e8
QuanDellXPS13 <98805733+quan11304@users.noreply.github.com>
2024-01-25 09:55:59 +0700

    L1E1 - Basic input/output functions

---
+++ L1E1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	char X[];
+	char Y[];
+	char Z[];
+	int n;
+	
+	printf("Enter X: ");
+	scanf("%s",X);
+	printf("Enter Y: ");
+	scanf("%s",Y);
+	printf("Enter Z: ");
+	scanf("%s",Z);
+	printf("Enter n: ");
+	scanf("%d",&n);
+}
