14a80245ae93a89573852c8dc42c6b3eee190441
QuanOnThePC <98805733+quan11304@users.noreply.github.com>
2024-02-28 21:30:33 +0700

    L3E1 - Modular multiplicative inverse function added.

--- Labwork 3/main.c
+++ Labwork 3/main.c
@@ -9,8 +9,8 @@ int modulus(int m, int e, int n) {
 
     for (int i = 1 << ((int) log2(e) - 1); i > 0; i >>= 1) {
         // Set i to 2^(length-1) and divide it by 2 every loop
-        // (int) log2(e) returns the length (base 0) of e in binary
-        // Length (Base 0, i.e. start counting from 0) = Length + 1 (Base 1)
+        // (int) log2(e) returns the length (base 0) of the binary representation of e
+        // Length (base 0, i.e. start counting from 0) = Length + 1 (in base 1)
         rm *= rm;
         rm %= n;
         if (e & i) { // Checking if the current bit in the exponent is a 1 or not
@@ -21,38 +21,54 @@ int modulus(int m, int e, int n) {
     return rm;
 }
 
+int modMulInv(int e, int m) {
+    // Output d so that (e*d) % m = 1
+    for (int d = 0; d < m; d++) {
+        if ((e * d) % m == 1)
+            return d;
+    }
+    return 0;
+}
+
 int main() {
     int p, q, e, d, m, c;
     // p and q is prime
     // e is relatively prime to (p-1)(q-1)
     char choice1, choice2;
 
-    printf("p = ");
-    scanf("%d", &p);
-    printf("q = ");
-    scanf("%d", &q);
-    fflush(stdin);
-
     int verify = 0;
-    do {
-        printf("Choose encryption (e) or decryption (d) exponent: ");
-        scanf("%c", &choice1);
-        switch (choice1) {
-            case 'e':
-            case 'E':
-                printf("e = ");
-                scanf("%d",&e);
-                break;
-            case 'd':
-            case 'D':
-                printf("d = ");
-                scanf("%d",&d);
-                break;
-            default:
-                printf("Please choose between 'e' and 'd'.");
-                verify = 1;
-        }
-    } while (verify);
+    while (1) {
+        printf("p = ");
+        scanf("%d", &p);
+        printf("q = ");
+        scanf("%d", &q);
+        fflush(stdin);
+
+        do {
+            printf("Choose encryption (e) or decryption (d) exponent: ");
+            scanf("%c", &choice1);
+            switch (choice1) {
+                case 'e':
+                case 'E':
+                    printf("e = ");
+                    scanf("%d", &e);
+                    d = modMulInv(e, (p - 1) * (q - 1));
+                    break;
+                case 'd':
+                case 'D':
+                    printf("d = ");
+                    scanf("%d", &d);
+                    e = modMulInv(d, (p - 1) * (q - 1));
+                    break;
+                default:
+                    printf("Please choose between 'e' and 'd'.");
+                    verify = 1;
+            }
+        } while (verify);
+
+        if (e == 0 || d == 0)
+            break;
+    }
 
     do {
         printf("Choose encryption or decryption process: ");
@@ -63,14 +79,14 @@ int main() {
             case 'm':
             case 'M':
                 printf("M = ");
-                scanf("%d",&m);
+                scanf("%d", &m);
                 break;
             case 'd':
             case 'D':
             case 'c':
             case 'C':
                 printf("d = ");
-                scanf("%d",&c);
+                scanf("%d", &c);
                 break;
             default:
                 printf("Please choose between 'e' (or 'M') and 'd' (or 'C').");
